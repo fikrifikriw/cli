38e1897afc9eae4494c499c3305b43113e8c0a77
Teodora Sandu <teodora.sandu@snyk.io>
2022-10-21 13:55:48 +0100

    refactor: test flow

--- src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/local-cache/policy-engine/constants/utils.ts
@@ -1,12 +1,12 @@
 import * as os from 'os';
 
 const policyEngineChecksums = `
-04fe9c50afad4ab91815281930f142d17e4047d0187564fffa81d2a04e292915  snyk-iac-test_0.35.1_Windows_x86_64.exe
-2876226d01d3b8c906bc1835d8f03ebb7b809b8a3bbce3e743d3301df86664b5  snyk-iac-test_0.35.1_Linux_x86_64
-36dbb048fc79669a7ae4924079a44248eeed83a5e785fa3877bb275d28f7be42  snyk-iac-test_0.35.1_Darwin_arm64
-4d08c4fe6027685681171903669372323f6018083bcc67ce804fd4a939ed4859  snyk-iac-test_0.35.1_Windows_arm64.exe
-4da012c52785749e24695d8b7f70782697afb06cc0125b6e3d062ba19d65b479  snyk-iac-test_0.35.1_Linux_arm64
-d6e017549e2806df32ecd61052d5f7489cca213a437af68d7e982b6200f8f81a  snyk-iac-test_0.35.1_Darwin_x86_64
+101c5a91ef7bc8918592e4536d5439d03f8a7586b278f058feb33240b361519e  snyk-iac-test_0.35.2_Windows_x86_64.exe
+20f471458a1e92c1365cdb31af1c7cfc4b34e5bb1f6ebbbadba86be6d7bf4819  snyk-iac-test_0.35.2_Darwin_x86_64
+45a3267eae64a71d74985aef751e265ddc6f363d6eceeef9bd01c2a9c2d34f96  snyk-iac-test_0.35.2_Linux_arm64
+4ceb48a4151f2b488cd5fbbd15a32f15acbf4b8d3c09ea32a88f4d5b5070bb97  snyk-iac-test_0.35.2_Linux_x86_64
+817be2b3e3babd526f261b5b7714c0a97288d8fe98fca6b702eb26c17a382c39  snyk-iac-test_0.35.2_Darwin_arm64
+a5383697a029eaf96d1b7775ac7e37839b43b2b8161b8ba063ae9315c7bef3ac  snyk-iac-test_0.35.2_Windows_arm64.exe
 `;
 
 export const policyEngineVersion = getPolicyEngineVersion();
